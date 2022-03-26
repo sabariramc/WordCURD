d3505bdea72f04bf2292bea84d27274fa3821c54
Sabariram <sabariram.c@gmail.com>
2022-03-26 07:28:16 +0530

    Added test for serving reqest

--- tests/server_test.go
+++ tests/server_test.go
@@ -1,11 +1,27 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/sabariramc/WordCURD/pkg/app"
+	"sabariram.com/goserverbase.git/utils"
 )
 
 func TestServerRequest(t *testing.T) {
-	srv := app.NewApp()
+	srv, err := app.NewApp()
+	if err != nil {
+		t.Fatal(err)
+	}
+	body := map[string]string{
+		"Word": "test",
+	}
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(body)
+	req := httptest.NewRequest("POST", "/tenant", &buf)
+	req.Header.Set("x-api-key", utils.GetEnv("TEST_API_KEY", ""))
+	w := httptest.NewRecorder()
+	srv.ServeHTTP(w, req)
 }
