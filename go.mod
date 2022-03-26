5a0cdc9c894bc917e5403b4e0c075589f92c7d65
Sabariram <sabariram.c@gmail.com>
2022-03-26 07:23:02 +0530

    Added test for creating new server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sabariramc/WordCURD
+
+go 1.18
---
+++ tests/server_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/sabariramc/WordCURD/pkg/app"
+)
+
+func TestServerRequest(t *testing.T) {
+	srv := app.NewApp()
+}
